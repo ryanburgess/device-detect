cc7a555b7967d33257efcf4355cb3a786c4f1f1a
Ryan <rburgess@evernote.com>
2015-01-26 20:43:58 -0800

    update documentation

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 Device Detect
 =============
 JavaScript module to check if user agent to return what device a user is on.
+
+## Install
+
+```js
+npm install device-detect --save-dev
+```
+
+## Use
+
+```js
+var deviceDetect = require('device-detect');
+
+// return all device and browser object
+var deviceDetect = deviceDetect();
+
+// result example:
+/* {
+  "device":"Macintosh",
+  "browser":"Chrome"
+} */
+```
  
 ## Release History
+* 1.0.1: Update documentation.
 * 1.0.0: Initial release.
  
 ## Contributing
--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-module.exports = function(){
-}
+module.exports = function (){
+  var ua  = window.navigator.userAgent,
+    device,
+    browser,
+    data ={};
+
+  // get device
+  if(ua.match(/(iPhone)/)){
+    device = 'iPhone';
+  }else if(ua.match(/(iPad)/)){
+    device = 'iPad';
+  }else if(ua.match(/(iPod)/)){
+    device = 'iPod';
+  }else if(ua.match(/(BlackBerry|BB10)/)){
+    device = 'BlackBerry';
+  }else if(ua.match(/(Android)/)){
+    device = 'Android';
+  }else if(ua.match(/(Macintosh)/)){
+    device = 'Macintosh';
+  }else if(ua.match(/(Windows)/)){
+    device = 'Windows';
+  }
+
+  // get browser
+  if (ua.indexOf('OPR') > 0 || ua.indexOf('Opera') > 0) {
+    browser = 'Opera';
+  }else if(ua.indexOf('Chrome') > 0){
+    browser = 'Chrome';
+  }else if(ua.indexOf('Firefox') > 0){
+    browser = 'Firefox';
+  }else if(ua.indexOf('Safari') > 0){
+    browser = 'Safari';
+  }else if(ua.indexOf('MSIE') > 0){
+    browser = 'IE';
+  }
+
+  // create object
+  data = {
+    device:  device,
+    browser: browser
+  };
+  return data;
+};
